2ed1d0e216001a1454093d571a28588746126702
GoldenKacper <kacperjagodzinski@op.pl>
2023-04-18 13:18:03 +0200

    Update

--- TestProject/BallTest.cs
+++ TestProject/BallTest.cs
@@ -21,12 +21,12 @@ namespace TestProject
         _ballsManager.UpdateBallStatus(0, 20, 30);
             Assert.AreEqual(_ballsManager.GetBall(0).X, 20);
             Assert.AreEqual(_ballsManager.GetBall(0).Y, 30);
-            
-        Random random = new Random();
-        _ballsManager.CreateNewBall(random.Next(1127) + 25, random.Next(404) + 25, 25);
+
+            Random random = new Random();
+            _ballsManager.CreateNewBall(random.Next(1127) + 25, random.Next(404) + 25, 25);
             //czy kula powstanie w prawid³owym obszarze(wymiary odpowiadaj¹ce scenie)
             Assert.IsTrue(_ballsManager.GetBall(1).X > 0 && _ballsManager.GetBall(1).X < 1190);
-            Assert.IsTrue(_ballsManager.GetBall(1).Y > 0 && _ballsManager.GetBall(1).X < 474);
+            Assert.IsTrue(_ballsManager.GetBall(1).Y > 0 && _ballsManager.GetBall(1).Y < 474);
 
 
         }
