8e82b337ba48839ebb86cd6d46878ec43e7b6bbb
242440 Adam Kruszynski <242440@edu.p.lodz.pl>
2023-04-18 18:27:32 +0200

    Stage 1 Final

--- TestProject/BallTest.cs
+++ TestProject/BallTest.cs
@@ -9,7 +9,7 @@ namespace TestProject
 
     {
         [TestMethod]
-        public void BallTests()
+        public void CreateBallTest()
         {
         IBallsManager _ballsManager = new BallsManager();
         _ballsManager.CreateNewBall(10, 10, 25);
@@ -17,18 +17,28 @@ namespace TestProject
             Assert.AreEqual(_ballsManager.GetBall(0).X, 10);
             Assert.AreEqual(_ballsManager.GetBall(0).Y, 10);
             Assert.AreEqual(_ballsManager.GetBall(0).Radius, 25);
+        }
+
+        [TestMethod]
+        public void UpdateBallTest()
+        {
+            IBallsManager _ballsManager = new BallsManager();
+            _ballsManager.CreateNewBall(10, 10, 25);
             //czy zmiana miejsca kuli dzia³a prawid³owo
-        _ballsManager.UpdateBallStatus(0, 20, 30);
+            _ballsManager.UpdateBallStatus(0, 20, 30);
             Assert.AreEqual(_ballsManager.GetBall(0).X, 20);
             Assert.AreEqual(_ballsManager.GetBall(0).Y, 30);
+        }
 
+        [TestMethod]
+        public void RandomBallsTest()
+        {
+            IBallsManager _ballsManager = new BallsManager();
             Random random = new Random();
             _ballsManager.CreateNewBall(random.Next(1127) + 25, random.Next(404) + 25, 25);
             //czy kula powstanie w prawid³owym obszarze(wymiary odpowiadaj¹ce scenie)
-            Assert.IsTrue(_ballsManager.GetBall(1).X > 0 && _ballsManager.GetBall(1).X < 1190);
-            Assert.IsTrue(_ballsManager.GetBall(1).Y > 0 && _ballsManager.GetBall(1).Y < 474);
-
-
+            Assert.IsTrue(_ballsManager.GetBall(0).X > 0 && _ballsManager.GetBall(0).X < 1190);
+            Assert.IsTrue(_ballsManager.GetBall(0).Y > 0 && _ballsManager.GetBall(0).Y < 474);
         }
     }
 }
---
+++ TestProject/LogicTest.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using Logic;
+using System;
+
+namespace TestProject
+{
+    [TestClass()]
+    public class LogicTest
+
+    {
+        [TestMethod]
+        public void InitializeTest()
+        {
+            IBallsManager _ballsManager = new BallsManager();
+            ILogic _logic = new Logic.Logic(_ballsManager);
+
+            _logic.Initialize(1190, 474, 5);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.IsTrue(_logic.GetLocation(i).X > 0 && _logic.GetLocation(i).X < 1190);
+                Assert.IsTrue(_logic.GetLocation(i).Y > 0 && _logic.GetLocation(i).Y < 474);
+            }
+        }
+    }
+}
